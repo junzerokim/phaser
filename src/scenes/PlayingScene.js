1f00b9629f20b84e8ae63e019ee9f8b4d5baf1a3
Cos Kim <junzerokim@gmail.com>
2023-05-27 23:44:10 +0900

    Feat: Player Move

--- src/scenes/PlayingScene.js
+++ src/scenes/PlayingScene.js
@@ -30,5 +30,47 @@ export default class PlayingScene extends Phaser.Scene {
 
     // PlayingScene의 background를 설정합니다.
     setBackground(this, 'background1');
+
+    this.m_cursorKeys = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    this.movePlayerManager(this.m_cursorKeys, this.m_player);
+  }
+
+  // player가 움직이도록 해주는 함수
+  movePlayerManager(keyboard, player) {
+    // 이동 키가 눌려있으면 player_anim 애니메이션을 재생
+    // 이동 키가 눌려있지 않으면 player_idle 애니메이션을 재생
+    if (keyboard.left.isDown || keyboard.right.isDown || keyboard.up.isDown || keyboard.down.isDown) {
+      if (!player.m_moving) {
+        player.play('player_anim');
+      }
+      player.m_moving = true;
+    } else {
+      if (player.m_moving) {
+        player.play('player_idle');
+      }
+      player.m_moving = false;
+    }
+
+    // vector를 사용해 움직임을 관리
+    // vector = [x, y]
+    // 왼쪽 키가 눌리면 [0] + 1, 오른쪽 키가 눌리면 [1] + 1
+    let vector = [0, 0];
+    if (keyboard.left.isDown) {
+      vector[0] -= 1;
+    } else if (keyboard.right.isDown) {
+      vector[0] += 1;
+    }
+
+    if (keyboard.up.isDown) {
+      vector[1] -= 1;
+    } else if (keyboard.down.isDown) {
+      vector[1] += 1;
+    }
+
+    // vector를 player의 파라미터로 넘김
+    player.move(vector);
   }
 }
